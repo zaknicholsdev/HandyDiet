cab0097d34df83ee82b415a09d0683753156e8b8
Zak Nichols <zaknicholsdev@gmail.com>
2020-06-20 23:05:15 -0400

    Updates

---
+++ precache-manifest.c398ab22498d6b99ec7ff905e2c624fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c99610a6c088526991306bde882474",
+    "url": "/HandyDiet/index.html"
+  },
+  {
+    "revision": "5993b91d530cf24ee3b9",
+    "url": "/HandyDiet/static/css/main.f0501915.chunk.css"
+  },
+  {
+    "revision": "b33da0048c843f3e87a1",
+    "url": "/HandyDiet/static/js/2.40b39512.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HandyDiet/static/js/2.40b39512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5993b91d530cf24ee3b9",
+    "url": "/HandyDiet/static/js/main.6f9ee896.chunk.js"
+  },
+  {
+    "revision": "3eab3e7a750a91da74a2",
+    "url": "/HandyDiet/static/js/runtime-main.dbb80488.js"
+  }
+]);
